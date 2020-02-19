c7ca0308d56e10f8e7afb49d59ba8679b2756b8b
Daren May <daren.may@gmail.com>
2020-02-18 21:05:42 -0700

    fixing linting issues

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import * as core from '@actions/core';
-import * as tc from '@actions/tool-cache';
-import * as exec from '@actions/exec';
-import * as path from 'path';
-import * as fs from 'fs';
-import { ExecOptions } from '@actions/exec/lib/interfaces';
+import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
+import * as exec from '@actions/exec'
+import * as path from 'path'
+import * as fs from 'fs'
+import {ExecOptions} from '@actions/exec/lib/interfaces'
 
-async function run() {
+async function run(): Promise<void> {
   try {
-
     // VSTest is not a tool we download
     // as Visual Studio is installed on Windows machines
     // However we need to download & use VSWhere to tell us
@@ -15,79 +14,92 @@ async function run() {
 
     // Triple check it's Windows process
     // Can't install VSWhere.exe for Ubuntu image etc..
-    const IS_WINDOWS = process.platform === 'win32';
-    if(IS_WINDOWS === false){
-      core.setFailed("VSTest.console.exe only works for Windows.");
-      return;
+    const IS_WINDOWS = process.platform === 'win32'
+    if (IS_WINDOWS === false) {
+      core.setFailed('VSTest.console.exe only works for Windows.')
+      return
     }
 
     // Try & find tool in cache
-    let directoryToAddToPath:string;
-    directoryToAddToPath = await tc.find("vswhere", "2.7.1");
+    //let directoryToAddToPath: string
+    const directoryToAddToPath = await tc.find('vswhere', '2.7.1')
 
-    if(directoryToAddToPath){
-      core.debug(`Found local cached tool at ${directoryToAddToPath} adding that to path`);
+    if (directoryToAddToPath) {
+      core.debug(
+        `Found local cached tool at ${directoryToAddToPath} adding that to path`
+      )
 
-      var VSTestPath = await FindVSTest(directoryToAddToPath);
-      core.debug(`VSTestPath == ${VSTestPath}`);
+      const VSTestPath = await FindVSTest(directoryToAddToPath)
+      core.debug(`VSTestPath == ${VSTestPath}`)
 
       // Add folder where VSTest lives to the PATH
-      await core.addPath(VSTestPath);
-      return;
+      await core.addPath(VSTestPath)
+      return
     }
 
     // Download VSWhere 2.7.1 release
-    core.debug("Downloading VSWhere v2.7.1 tool");
-    const vsWherePath = await tc.downloadTool("https://github.com/microsoft/vswhere/releases/download/2.7.1/vswhere.exe");
+    core.debug('Downloading VSWhere v2.7.1 tool')
+    const vsWherePath = await tc.downloadTool(
+      'https://github.com/microsoft/vswhere/releases/download/2.7.1/vswhere.exe'
+    )
 
     // Rename the file which is a GUID without extension
-    var folder = path.dirname(vsWherePath);
-    var fullPath = path.join(folder, "vswhere.exe");
-    fs.renameSync(vsWherePath, fullPath);
+    const folder = path.dirname(vsWherePath)
+    const fullPath = path.join(folder, 'vswhere.exe')
+    fs.renameSync(vsWherePath, fullPath)
 
     //Cache the directory with VSWhere in it - which returns a NEW cached location
-    var cachedToolDir = await tc.cacheDir(folder, "vswhere", "2.7.1");
-    core.debug(`Cached Tool Dir ${cachedToolDir}`);
+    const cachedToolDir = await tc.cacheDir(folder, 'vswhere', '2.7.1')
+    core.debug(`Cached Tool Dir ${cachedToolDir}`)
 
-    var VSTestPath = await FindVSTest(cachedToolDir);
-    core.debug(`VSTestPath == ${VSTestPath}`);
+    const VSTestPath = await FindVSTest(cachedToolDir)
+    core.debug(`VSTestPath == ${VSTestPath}`)
 
     // Add folder where VSTest lives to the PATH
-    await core.addPath(VSTestPath);
-
+    await core.addPath(VSTestPath)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
 
-run();
+run()
 
+async function FindVSTest(pathToVSWhere: string): Promise<string> {
+  let vsTestPath = ''
 
-async function FindVSTest(pathToVSWhere:string):Promise<string>{
-
-  var vsTestPath = "";
-
-  const options:ExecOptions = {};
+  const options: ExecOptions = {}
   options.listeners = {
     stdout: (data: Buffer) => {
-      var output = data.toString();
-      vsTestPath += output;
+      const output = data.toString()
+      vsTestPath += output
     }
-  };
-
-  // Run VSWhere to tell us where VSTest is
-  var vsWhereExe = path.join(pathToVSWhere, "vswhere.exe");
-  await exec.exec(vsWhereExe, ['-latest', '-requires', 'Microsoft.Component.VSTest', '-find', 'VSTest\\**\\Bin\\VSTest.exe'], options);
-
-  if(vsTestPath === ""){
-    core.setFailed("Unable to find VSTest.exe");
   }
 
-  vsTestPath += "\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow\\vstest.console.exe"
+  // Run VSWhere to tell us where VSTest is
+  const vsWhereExe = path.join(pathToVSWhere, 'vswhere.exe')
+  await exec.exec(
+    vsWhereExe,
+    [
+      '-latest',
+      '-products',
+      'Microsoft.VisualStudio.Workload.ManagedDesktop',
+      '-requiresAny',
+      '-property',
+      'installationPath'
+    ],
+    options
+  )
 
-  var folderForVSTest = path.dirname(vsTestPath)
-  core.debug(`VSTest = ${vsTestPath}`);
-  core.debug(`Folder for VSTest ${folderForVSTest}`);
+  if (vsTestPath === '') {
+    core.setFailed('Unable to find VSTest.exe')
+  }
 
-  return folderForVSTest;
+  vsTestPath +=
+    '\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow\\vstest.console.exe'
+
+  const folderForVSTest = path.dirname(vsTestPath)
+  core.debug(`VSTest = ${vsTestPath}`)
+  core.debug(`Folder for VSTest ${folderForVSTest}`)
+
+  return folderForVSTest
 }
